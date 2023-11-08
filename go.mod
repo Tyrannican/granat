9f2c703f7c2e1c5959daf5ccd79d190cae76d92d
Graham Keenan <graham.keenan01@bbc.co.uk>
2023-11-08 18:42:35 +0000

    added skeleton

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tyrannican/granat
+
+go 1.21.1
---
+++ granat.go
@@ -0,0 +1,10 @@
+package granat
+
+import (
+	store "github.com/tyrannican/granat/stores"
+)
+
+type GlobalStore struct {
+    stringStore *store.StringStore
+}
+
