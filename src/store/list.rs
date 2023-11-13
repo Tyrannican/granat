9a1bd41a60fd1e44833094c2fb449be4c59baf89
Graham Keenan <graham.keenan01@bbc.co.uk>
2023-11-13 22:57:51 +0000

    work: working on implementation of the ListStore

--- src/store/list.rs
+++ src/store/list.rs
@@ -1,21 +1,32 @@
+use anyhow::Result;
 use serde::{Serialize, Deserialize};
 
-use crate::store::EntryValue;
+use crate::store::{EntryValue, StoreMode};
 
 use std::collections::{HashMap, LinkedList};
 
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ListStore {
-    pub store: HashMap<String, LinkedList<EntryValue>>
+    pub store: HashMap<String, LinkedList<EntryValue>>,
+
+    #[serde(skip)]
+    pub mode: StoreMode
 }
 
 impl ListStore {
     pub fn new() -> Self {
         return Self {
-            store: HashMap::new()
+            store: HashMap::new(),
+            mode: StoreMode::default()
         };
     }
 
+    pub fn mode(&mut self, mode: StoreMode) {
+        self.mode = mode;
+    }
 
+    pub fn push(&mut self, key: String, value: String) -> Result<()> {
+        return Ok(())
+    }
 }
